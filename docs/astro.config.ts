55f67c4c5f7938e6f4ef2ff4162b915a8012d0fe
trueberryless <trueberryless+github@gmail.com>
2024-11-05 12:38:39 +0100

    create plugin
    deploy

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,24 +1,24 @@
-import starlight from '@astrojs/starlight'
-import { defineConfig } from 'astro/config'
-import starlightContributorList from 'starlight-contributor-list'
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  integrations: [
-    starlight({
-      editLink: {
-        baseUrl: 'https://github.com/trueberryless-org/starlight-contributor-list/edit/main/docs/',
-      },
-      plugins: [starlightContributorList()],
-      sidebar: [
-        {
-          label: 'Start Here',
-          items: [{ slug: 'getting-started' }],
-        },
-      ],
-      social: {
-        github: 'https://github.com/trueberryless-org/starlight-contributor-list',
-      },
-      title: 'starlight-contributor-list',
-    }),
-  ],
-})
+    integrations: [
+        starlight({
+            editLink: {
+                baseUrl:
+                    "https://github.com/trueberryless-org/starlight-contributor-list/edit/main/docs/",
+            },
+            customCss: ["./src/styles/custom.css"],
+            sidebar: [
+                {
+                    label: "Start Here",
+                    items: [{ slug: "getting-started" }, { slug: "demo" }],
+                },
+            ],
+            social: {
+                github: "https://github.com/trueberryless-org/starlight-contributor-list",
+            },
+            title: "Starlight Contributor List",
+        }),
+    ],
+});
