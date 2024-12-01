b7276563987dbec1cc8707efda66e56a1c71629c
trueberryless <99918022+trueberryless@users.noreply.github.com>
2024-12-01 20:51:58 +0000

    [ci] format

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -3,33 +3,34 @@ import { defineConfig } from "astro/config";
 import starlightPluginsDocsComponents from "@trueberryless-org/starlight-plugins-docs-components";
 
 export default defineConfig({
-    integrations: [
-        starlight({
-            title: "Starlight Contributor List",
-            logo: {
-                light: "./src/assets/logo-light.png",
-                dark: "./src/assets/logo-dark.png",
-                replacesTitle: true,
-            },
-            editLink: {
-                baseUrl:
-                    "https://github.com/trueberryless-org/starlight-contributor-list/edit/main/docs/",
-            },
-            customCss: ["./src/styles/custom.css"],
-            sidebar: [
-                {
-                    label: "Start Here",
-                    items: [{ slug: "getting-started" }, { slug: "demo" }],
-                },
-            ],
-            social: {
-                github: "https://github.com/trueberryless-org/starlight-contributor-list",
-            },
-            plugins: [
-                starlightPluginsDocsComponents({
-                    pluginName: "starlight-contributor-list",
-                }),
-            ],
+  integrations: [
+    starlight({
+      title: "Starlight Contributor List",
+      logo: {
+        light: "./src/assets/logo-light.png",
+        dark: "./src/assets/logo-dark.png",
+        replacesTitle: true,
+      },
+      editLink: {
+        baseUrl:
+          "https://github.com/trueberryless-org/starlight-contributor-list/edit/main/docs/",
+      },
+      customCss: ["./src/styles/custom.css"],
+      sidebar: [
+        {
+          label: "Start Here",
+          items: [{ slug: "getting-started" }, { slug: "demo" }],
+        },
+      ],
+      social: {
+        github:
+          "https://github.com/trueberryless-org/starlight-contributor-list",
+      },
+      plugins: [
+        starlightPluginsDocsComponents({
+          pluginName: "starlight-contributor-list",
         }),
-    ],
+      ],
+    }),
+  ],
 });
